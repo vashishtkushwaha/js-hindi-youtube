4c10f929f361c76f84acdb466956a2d3ab8baaef
vashishtkushwaha <kkroy2077@gmail.com>
2024-08-13 02:23:27 +0000

    variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountid = 144553
+let accountEmail = "manishkumr2@gmail.com"
+var accountpassword = "123345"
+accountcity = "jaypur"
+
+//accountId =2 // not allowed
+
+accountEmail = "manishkar703072@gmail.com"
+accountpassword =" 45632"
+accountcity = "bihar"
+
+/*
+prefer not to use var
+because of issu in block scope and functional scope
+*/
+
+console.log(accountid);
+
+
+console.table([accountid,accountEmail,accountpassword,accountcity,])
